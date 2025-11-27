cd6335157653f9ddff178df1da2310debf2de073
Sandip Kumar Rath <149088599+sandipcomrade@users.noreply.github.com>
2025-11-27 12:37:39 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+class Main{
+    public static void main(String[] args){
+        System.out.println("Hii i am learning jenkins newly");
+        System.out.println("Running Time : - "+new Date());
+    }
+}
